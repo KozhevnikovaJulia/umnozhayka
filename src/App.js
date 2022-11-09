1d7c1b60a5ca61fe31165dd6753f0fa3ff61f85f
JuliaKozhevnikova <Kozhevnikova1501@yandex.ru>
2022-11-09 14:38:37 +0300

    +redirect

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Header } from './ui/components/Header/Header'
 import { HomePage } from './ui/pages/HomePage/HomePage'
 import { SettingsPage } from './ui/pages/SettingsPage/SettingsPage'
 import { TestPage } from './ui/pages/TestPage/TestPage'
-import { Routes ,Route } from 'react-router-dom'
+import { Routes ,Route, Navigate } from 'react-router-dom'
 
 export const App = () => {
   return (
@@ -14,6 +14,7 @@ export const App = () => {
            <Route path='/' element={<HomePage/>} />
            <Route path='/settings' element={<SettingsPage/>} />
            <Route path='/test' element={<TestPage/>} />
+           <Route path='*' element={<Navigate replace to="/" />} />
        </Routes> 
     </div>
   );
